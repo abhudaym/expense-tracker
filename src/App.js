3619d93c530ce86147addfeee5d36e685eca282b
abhudaym <abhuday.mishra@hotmail.com>
2020-05-13 19:00:30 +0530

    Completed the front-end and working of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import {GlobalContextProvider} from './context/GlobalContext';
+
 import './App.css';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <GlobalContextProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalContextProvider>
   );
 }
 
